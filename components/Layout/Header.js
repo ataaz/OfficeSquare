7a4daa8d401eaf49d5f5c6115d64d8c34c715233
ataaz <atashaikh@live.com>
2024-01-11 11:38:22 +0500

    update for locations

--- components/Layout/Header.js
+++ components/Layout/Header.js
@@ -159,6 +159,7 @@ export default function Header() {
                             :
                             <svg fill="#000000" style={{marginLeft:`5px`,position:`relative`,top:`3px`}} version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" width="20px" height="20px" viewBox="0 0 100 100" enableBackground="new 0 0 100 100" xmlSpace="preserve"> <g> <path d="M78.466,35.559L50.15,63.633L22.078,35.317c-0.777-0.785-2.044-0.789-2.828-0.012s-0.789,2.044-0.012,2.827L48.432,67.58 c0.365,0.368,0.835,0.563,1.312,0.589c0.139,0.008,0.278-0.001,0.415-0.021c0.054,0.008,0.106,0.021,0.16,0.022 c0.544,0.029,1.099-0.162,1.515-0.576l29.447-29.196c0.785-0.777,0.79-2.043,0.012-2.828S79.249,34.781,78.466,35.559z"/> </g> </svg>
                             }</a></li>
+                            <li className="nav-item list-inline-item"><Link className="nav-link" aria-current="page" href="locations">Locations</Link></li>
                             {/* <li className="nav-item list-inline-item"><Link className="nav-link" aria-current="page" href="">Blog</Link></li> */}
                             <li className="nav-item list-inline-item HdrBtn"><Link className="nav-link" href="contact-us">
 <svg id="Layer_1" className="bookUR" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 688.26 20.42">
